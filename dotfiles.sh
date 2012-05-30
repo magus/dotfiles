a9214640a2b3a4a4a370b1221e1501093366f339
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2012-05-30 11:21:56 -0500

    notification functions

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
 # bootstrap.sh
 # set up the initial zsh and some dependencies
 # make dem dotfiles
 
+function e_header()   { echo -e "\n\n\033[1;35m(STAR UNICODE CHARACTER)\033[0m  $@"; }
+function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
+function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
+function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
+
+
+#git?
+if [[ "$(which git | grep -w 'not found')" ]]; then
+	e_arrow 'git not installed'
+else
+	e_success 'git installed'
+fi
 
