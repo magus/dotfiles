5011ef402157b6f52559c9188008998d5573c723
noah <noah@iamnoah.com>
2012-05-30 10:17:42 -0500

    change osx.zsh > path.zsh to reflect purpose

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,10 +4,11 @@ function prepend_path () {
 
 #prepend_path /usr/local/share/npm/bin
 
+#npm modules
+export NODE_PATH=/usr/local/lib/node_modules:$PATH
+
 #apple put /usr/bin before /usr/local/bin -_-
 prepend_path /usr/local/bin
 prepend_path /usr/local/sbin
 
-#npm modules
-export NODE_PATH=/usr/local/lib/node_modules:$PATH
 
