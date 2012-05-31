89d2322bcba20c3247fd7ce598b1a47e2541534f
noah <noah@iamnoah.com>
2012-05-31 11:32:53 -0500

    star header and style other notifs

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,11 +4,13 @@
 # set up the initial zsh and some dependencies
 # make dem dotfiles
 
-function e_header()   { echo -e "\n\n\033[1;35m(STAR UNICODE CHARACTER)\033[0m  $@"; }
-function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
-function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
-function e_arrow()    { echo -e " \033[1;33m➜\033[0m  $@"; }
+function e_header()   { echo -e "\n\n\033[1;35m☆\033[0m  $@"; }
+function e_success()  { echo -e " \033[1;32m ✔\033[0m  $@"; }
+function e_error()    { echo -e " \033[1;31m ✖\033[0m  $@"; }
+function e_arrow()    { echo -e " \033[1;33m ➜\033[0m  $@"; }
 
+e_header "HEADER"
+e_success "successful installation"
 
 #git?
 #if [[ "$(which git | grep -w 'not found')" ]]; then
