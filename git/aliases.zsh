722b9f63ca09e7ffff159a64af219f62f0812152
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-01-11 04:15:09 -0800

    git;gall: execute argument command on all git repos in dir executes an arbitrary commands passed as an argument on any git repos in the directory context script executes within. e.g. gall git status > runs git status on any git repositories found in current directory

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,3 +4,4 @@
 
 #git aliases
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+gall() { find . -type d -follow -exec sh -c "(cd {} && [[ -d .git ]] && echo {} && $*)" ';'; }
