6575a72237a93f05ae9685834ef9e77a3d6c1ea3
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-01-12 23:17:21 -0800

    detect ip address for first time reminder

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -3,7 +3,7 @@ source ~/.zshrc
 
 cat <<EOF
 SSH Keys (if this is a server)
- 1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
+ 1. (main) scp ~/.ssh/id_rsa.pub $USER@$(myip):~/.ssh/
  2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 Git / GitHub
