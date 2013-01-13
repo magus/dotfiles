4a40f5df916ec4a74b10239d1949ad0ca6a06c47
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-01-12 23:17:21 -0800

    detect ip address for first time reminder

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@
 alias gogogadget='npm install'
 alias hakase='ssh hakase.cloudapp.net'
 alias ec2='ssh ubuntu@23.21.196.120 -i ~/.ssh/air.pem'
+alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
