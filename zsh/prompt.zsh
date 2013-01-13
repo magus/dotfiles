e67ee6f2a0258455cf77eeff3e93c88c9a60b3cd
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-01-12 22:01:02 -0800

    update git status prompt, git status message changed

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,96 +5,96 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 git_branch() {
-	echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+    echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
 git_dirty() {
-	st=$(git status 2>/dev/null | tail -n 1)
-	if [[ $st == "" ]]
-	then
-		echo ""
-	else
-		if [[ $st == "nothing to commit (working directory clean)" ]]
+    st=$(git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]
     then
-			echo "%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+        echo ""
     else
-			echo "%{$fg_bold[red]%}$(git_prompt_info)*%{$reset_color%}"
+        if [[ "$st" =~ ^nothing ]]
+        then
+            echo "%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+        else
+            echo "%{$fg_bold[red]%}$(git_prompt_info)*%{$reset_color%}"
+        fi
     fi
-	fi
 }
 
 git_prompt_info () {
-	ref=$(git symbolic-ref HEAD 2>/dev/null) || return
-	# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-	echo "${ref#refs/heads/}"
+    ref=$(git symbolic-ref HEAD 2>/dev/null) || return
+    # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+    echo "${ref#refs/heads/}"
 }
 
 unpushed () {
-	git cherry -v @{upstream} 2>/dev/null
+    git cherry -v @{upstream} 2>/dev/null
 }
 
 need_push () {
-	if [[ $(unpushed) == "" ]]
-	then
-		echo ""
-	else
-		echo "%{$fg[magenta]%}%Bunpushed%b%{$reset_color%}"
-	fi
+    if [[ $(unpushed) == "" ]]
+    then
+        echo ""
+    else
+        echo "%{$fg[magenta]%}%Bunpushed%b%{$reset_color%}"
+    fi
 }
 
 
 git_prompt_color() {
-	st=$(git status 2>/dev/null | tail -n 1)
-	if [[ $st == "" ]]
-	then
-		echo "%{$fg_bold[yellow]%}⇨  %{$reset_color%}"
-	else
-		if [[ $st == "nothing to commit (working directory clean)" ]]
+    st=$(git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]
     then
-			if [[ $(unpushed) == "" ]]
-			then
-				echo "%{$fg_bold[green]%}⇨  %{$reset_color%}"
-			else
-				echo "%{$fg[magenta]%}⇨  %{$reset_color%}"
-			fi
+        echo "%{$fg_bold[yellow]%}⇨  %{$reset_color%}"
     else
-			echo "%{$fg_bold[red]%}⇨  %{$reset_color%}"
+        if [[ "$st" =~ ^nothing ]]
+        then
+            if [[ $(unpushed) == "" ]]
+            then
+                echo "%{$fg_bold[green]%}⇨  %{$reset_color%}"
+            else
+                echo "%{$fg[magenta]%}⇨  %{$reset_color%}"
+            fi
+        else
+            echo "%{$fg_bold[red]%}⇨  %{$reset_color%}"
+        fi
     fi
-	fi
 }
 
 #number of todo items
 #props @holman
 todo_num(){
-	if $(which todo &> /dev/null)
-	then
-		num=$(echo $(todo ls | wc -l))
-		let todos=num-2
-		if [ $todos != 0 ]
-		then
-			echo "%{$fg_bold[cyan]%}$todos%{$reset_color%}"
-		else
-			echo ""
-		fi
-	else
-		echo ""
-	fi
+    if $(which todo &> /dev/null)
+    then
+        num=$(echo $(todo ls | wc -l))
+        let todos=num-2
+        if [ $todos != 0 ]
+        then
+            echo "%{$fg_bold[cyan]%}$todos%{$reset_color%}"
+        else
+            echo ""
+        fi
+    else
+        echo ""
+    fi
 }
 
 user_name(){
-	echo "%{$fg_bold[green]%}%B%n%b%{$reset_color%}"
+    echo "%{$fg_bold[green]%}%B%n%b%{$reset_color%}"
 }
 
-host_name(){	
-	echo "%{$fg[yellow]%}%m%{$reset_color%}"
+host_name(){    
+    echo "%{$fg[yellow]%}%m%{$reset_color%}"
 }
 
 path_abbv(){
-	echo "%{$fg_bold[white]%}${PWD/#$HOME/~}%{$reset_color%}"
+    echo "%{$fg_bold[white]%}${PWD/#$HOME/~}%{$reset_color%}"
 }
 
 set_right_prompt () {
-	export RPROMPT="$(git_dirty) $(need_push) ($(todo_num))"
+    export RPROMPT="$(git_dirty) $(need_push) ($(todo_num))"
 }
 
 
@@ -105,8 +105,8 @@ set_right_prompt () {
 #executed just before each prompt
 
 precmd() {
-	title "zsh" "%m" "%55<...<%~"
-	set_right_prompt
+    title "zsh" "%m" "%55<...<%~"
+    set_right_prompt
 }
 
 #minimal prompt
