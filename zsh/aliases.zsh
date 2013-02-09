f5798c86b7c8457b72716cf9f288bc95c39a35d2
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-02-08 17:11:43 -0800

    add mio azure alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # aliases
 alias gogogadget='npm install'
 alias hakase='ssh hakase.cloudapp.net'
+alias mio='ssh mio.cloudapp.net'
 alias ec2='ssh ubuntu@23.21.196.120 -i ~/.ssh/air.pem'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
