bf6a16f480e57dcc16e95297f43f8633611f5648
noah <noah@iamnoah.com>
2013-03-09 17:03:31 +0800

    amazon and azure aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,5 +2,6 @@
 alias gogogadget='npm install'
 alias hakase='ssh hakase.cloudapp.net'
 alias mio='ssh mio.cloudapp.net'
-alias ec2='ssh ubuntu@23.21.196.120 -i ~/.ssh/air.pem'
+alias nano='ssh nano.cloudapp.net'
+alias ec2='ssh ubuntu@ec2-54-251-107-68.ap-southeast-1.compute.amazonaws.com -i ~/.ssh/ec2.pem'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
