b47bf27963dc2baa5ac62d9ab3017786dfa9678e
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2013-03-29 11:42:19 +0800

    new azure mio instance

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # aliases
 alias gogogadget='npm install'
 alias hakase='ssh hakase.cloudapp.net'
-alias mio='ssh mio.cloudapp.net'
+alias mio='ssh azureuser@mio.cloudapp.net -p 55626'
 alias nano='ssh nano.cloudapp.net'
 alias ec2='ssh ubuntu@54.251.47.214 -i ~/.ssh/ec2.pem'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
