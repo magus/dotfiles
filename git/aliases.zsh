f4b619a5fb1a34fbd1951ea1aa031b4ff749b6f6
noah <noah@iamnoah.com>
2015-03-30 10:29:12 -0700

    grm to use correct awk token for git rm

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@
 #hub breaks git autocomplete...fail
 #eval "$(hub alias -s)"
 
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grm="git status | grep deleted | awk '{print \$2}' | xargs git rm"
 
 # gall -- execute command on all git repos in current directory
 # e.g. gall status -s
