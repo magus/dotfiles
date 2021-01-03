324945229857cbd423218d9e69b1373cf0d4d09c
magus <noah@iamnoah.com>
2021-01-02 22:47:02 -0800

    cleanup double comments

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,20 +6,20 @@ function append_path () {
   export PATH=$PATH:$1
 }
 
-#prepend_path /usr/local/share/npm/bin
+# prepend_path /usr/local/share/npm/bin
 
-#npm modules
+# npm modules
 export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
 prepend_path /usr/local/share/npm/bin
 
-#apple put /usr/bin before /usr/local/bin -_-
+# apple put /usr/bin before /usr/local/bin -_-
 prepend_path /usr/local/bin
 prepend_path /usr/local/sbin
 
-### Added by the Heroku Toolbelt
+# Added by the Heroku Toolbelt
 prepend_path /usr/local/heroku/bin
 
-### Added by RVM for scripting
+# Added by RVM for scripting
 append_path $HOME/.rvm/bin
 
 # google cloud sdk
