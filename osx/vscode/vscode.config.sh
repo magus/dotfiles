b916d4cb2f922f3d836146a0d41cef667c84f485
magus <noah@iamnoah.com>
2021-12-18 15:31:21 -0800

    osx: symmlink vscode settings file

---
+++ osx/vscode/vscode.config.sh
@@ -0,0 +1,7 @@
+echo 'symmlink vscode pref file to user library application dir'
+
+src="$HOME/.dotfiles/osx/vscode/settings.json"
+tgt="$HOME/Library/Application Support/Code/User"
+
+echo "linking [$src] -> [$tgt]"
+ln -sf "$src" "$tgt"
