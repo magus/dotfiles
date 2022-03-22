95dc4c9d6b67cda6ed3a20ef549a29e17d7b4465
magus <noah@iamnoah.com>
2022-03-22 15:15:12 -0700

    kitty: ensure config dir exists

--- kitty/kitty.config.sh
+++ kitty/kitty.config.sh
@@ -1,5 +1,8 @@
 echo 'symmlink kitty conf files to default kitty conf dir (~/.config/kitty)'
 
+KITTY_CONFIG_DIR="$HOME/.config/kitty"
+mkdir -p "$KITTY_CONFIG_DIR"
+
 function copy_configs() {
   find $HOME/.dotfiles/kitty -iname '*.conf' | {
     local files=()
