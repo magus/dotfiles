fb0494b68741db551aee5c35d442e315114a2794
magus <noah@iamnoah.com>
2023-03-25 15:05:26 -0700

    zsh: python3 aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,7 @@
 alias gogogadget='npm install'
 alias hakase='ssh noah@hakase.cloudapp.net -p 55626 -i ~/.ssh/azure.key'
 alias ec2='ssh ubuntu@54.251.47.214 -i ~/.ssh/ec2.pem'
+
+# python
+alias python='/usr/local/bin/python3'
+alias pip='/usr/local/bin/pip3'
