5ea1b9a6664077626ecd15cec5b86692b91d8c5e
magus <noah@iamnoah.com>
2023-05-18 14:56:35 -0700

    homebrew early exit and check kitty

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -6,32 +6,35 @@
 # using Homebrew.
 
 # Check for Homebrew
-if [[ -e $(which brew) ]]; then
-  # Update homebrew
-  brew update
-  # Install homebrew packages
-  brew install grc coreutils grc nmap parallel iftop
-  brew install fpp
-  brew install git-delta
-  brew install direnv
-
-  # node
-  brew install fnm
-
-  # yarn
-  brew install yarn --ignore-dependencies
-
-  # vim
-  brew install vim
-
-  # kitty terminal
-  brew install --cask kitty
-
-else
+if [[ ! -e $(which brew) ]]; then
   echo ' '
   echo '  🚨 You should probably install Homebrew first:'
   echo ' '
   echo '     eval "$(curl https://raw.githubusercontent.com/magus/dotfiles/master/homebrew/install.sh)"'
   echo ' '
   exit 1
+fi
+
+
+# Update homebrew
+brew update
+
+# Install homebrew packages
+brew install grc coreutils grc nmap parallel iftop
+brew install fpp
+brew install git-delta
+brew install direnv
+
+# node
+brew install fnm
+
+# yarn
+brew install yarn --ignore-dependencies
+
+# vim
+brew install vim
+
+# kitty terminal
+if [[ ! -e $(which kitty) ]]; then
+  brew install --cask kitty
 fi
