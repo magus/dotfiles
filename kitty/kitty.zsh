2e5782aa41e97f3d8249ee7f915e0a12832cfe10
magus <noah@iamnoah.com>
2023-05-24 20:03:24 -0700

    kitty: only setup kitty if using kitty

--- kitty/kitty.zsh
+++ kitty/kitty.zsh
@@ -1,16 +1,19 @@
-
+#!/bin/bash
 # any custom commands to further setup kitty in zsh
 
-# Completion for kitty
-kitty + complete setup zsh | source /dev/stdin
+if check_bin "kitty"; then
+  # Completion for kitty
+  kitty + complete setup zsh | source /dev/stdin
 
-# display image in kitty
-# https://sw.kovidgoyal.net/kitty/kittens/icat.html
-alias icat="kitty +kitten icat"
+  # display image in kitty
+  # https://sw.kovidgoyal.net/kitty/kittens/icat.html
+  alias icat="kitty +kitten icat"
 
-# kitten for side-by-side diff
-alias diff="kitty +kitten diff"
+  # kitten for side-by-side diff
+  alias diff="kitty +kitten diff"
 
-# ssh override for kitty
-# https://sw.kovidgoyal.net/kitty/faq.html#i-get-errors-about-the-terminal-being-unknown-or-opening-the-terminal-failing-when-sshing-into-a-different-computer
-alias kssh="kitty +kitten ssh"
+  # ssh override for kitty
+  # https://sw.kovidgoyal.net/kitty/faq.html#i-get-errors-about-the-terminal-being-unknown-or-opening-the-terminal-failing-when-sshing-into-a-different-computer
+  alias kssh="kitty +kitten ssh"
+fi
+
