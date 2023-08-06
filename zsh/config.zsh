195467fb28134014bfff30e08641d90fea6cb448
magus <noah@iamnoah.com>
2023-08-05 23:56:14 -0700

    zsh: remove functions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,8 +5,6 @@ export LS_COLORS=$LSCOLORS
 
 fpath=($ZSH/zsh/functions $fpath)
 
-autoload -U $ZSH/zsh/functions/*(:t)
-
 setopt correct
 setopt completeinword
 setopt extendedglob		#complex globbing ftw
