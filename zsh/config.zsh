273e5a77aa85551baad0f8987a7d0192a87c4380
magus <noah@iamnoah.com>
2023-08-22 19:35:01 -0700

    zimfw: zsh plugin manager

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# your project folder that we can `c [tab]` to
+export PROJECTS=~/Development
+
+export EDITOR="/usr/local/bin/code"
 
 export CLICOLOR=true
 export LSCOLORS="exfxcxdxbxegedabagacad"
@@ -5,12 +11,22 @@ export LS_COLORS=$LSCOLORS
 
 fpath=($ZSH/zsh/functions $fpath)
 
+# remove path separator from WORDCHARS
+# this allows option + left and right arrow to jump folders in path
+# without this it will jump the entire path as one single word
+WORDCHARS=${WORDCHARS//[\/]}
+
+# prompt for spelling correction of commands.
 setopt correct
+# customize spelling correction prompt.
+SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
+
 setopt completeinword
 setopt extendedglob		#complex globbing ftw
 setopt promptsubst		#expansion/substitution in prompts
 
-#emacs line editing
+# set editor default keymap to emacs (`-e`) or vi (`-v`)
+# emacs line editing
 bindkey -e
 
 # `⌥ + ← →` to move back and forward words
@@ -20,3 +36,45 @@ bindkey -e
 #
 bindkey "^[^[[C" forward-word
 bindkey "^[^[[D" backward-word
+
+# zsh-users/zsh-history-substring-search
+zmodload -F zsh/terminfo +p:terminfo
+# bind ^[[A/^[[B manually so up/down works both before and after zle-line-init
+# previously was only doing below, before pulling in default config from zim
+#   bindkey '^[[A' history-substring-search-up
+#   bindkey '^[[B' history-substring-search-down
+#
+for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
+for key ('^[[B' '^N' ${terminfo[kcud1]}) bindkey ${key} history-substring-search-down
+for key ('k') bindkey -M vicmd ${key} history-substring-search-up
+for key ('j') bindkey -M vicmd ${key} history-substring-search-down
+unset key
+
+
+# used by zsh-history-substring-search
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+
+
+# zsh-users/zsh-autosuggestions
+# set the text color for the suggested text
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/README.md#configuration
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/README.md#suggestion-highlight-style
+# https://stackoverflow.com/questions/47310537/how-to-change-zsh-autosuggestions-color/53070834#53070834
+# https://github.com/zsh-users/zsh-autosuggestions/issues/12
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=242'
+
+# disable automatic widget re-binding on each precmd. This can be set when
+# zsh-users/zsh-autosuggestions is the last module in your ~/.zimrc.
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+
+
+# zsh-users/zsh-syntax-highlighting
+# set what highlighters will be used.
+# see https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+# customize the main highlighter styles.
+# see https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md#how-to-tweak-it
+# typeset -A ZSH_HIGHLIGHT_STYLES
+# ZSH_HIGHLIGHT_STYLES[comment]='fg=242'
