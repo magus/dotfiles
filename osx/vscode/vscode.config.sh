d329a9f4ef8d34179fc2c9ca50ae723a93db972e
magus <noah@iamnoah.com>
2023-09-06 12:14:49 -0700

    vscode: keybindings symmlink

--- osx/vscode/vscode.config.sh
+++ osx/vscode/vscode.config.sh
@@ -2,8 +2,13 @@
 
 echo 'symmlink vscode pref file to user library application dir'
 
-src="$HOME/.dotfiles/osx/vscode/settings.json"
-tgt="$HOME/Library/Application Support/Code/User"
+link_file() {
+  src="$1"
+  tgt="$2"
 
-echo "linking [$src] -> [$tgt]"
-ln -sf "$src" "$tgt"
+  echo "linking [$src] -> [$tgt]"
+  ln -sf "$src" "$tgt"
+}
+
+link_file "$HOME/.dotfiles/osx/vscode/settings.json" "$HOME/Library/Application Support/Code/User"
+link_file "$HOME/.dotfiles/osx/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User"
