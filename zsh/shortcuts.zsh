4e713e12e0ed3a182d7249bf80aaa0ed187d3952
magus <noah@iamnoah.com>
2023-11-10 16:58:11 -0800

    zsh/shortcuts: option+shift+c copy current buffer

--- zsh/shortcuts.zsh
+++ zsh/shortcuts.zsh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# discover key codes by using `cat -v` and pressing the keys
+
 
 function copy-command-line {
     echo -n $BUFFER | pbcopy
 }
 
 zle -N copy-command-line
-bindkey '^[[99;10u' copy-command-line # ⌘+shift+c
+
+# discover key codes by using `cat -v` and pressing the keys
+
+# previously used `⌘+shift+c` but that did not work in tmux
+bindkey 'Ç' copy-command-line # option+shift+c
