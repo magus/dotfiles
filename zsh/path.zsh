7a716d9c25ed779e40e9890ed66145864186ffe9
magus <noah@iamnoah.com>
2024-02-11 18:29:47 -0800

    path: remove unused node paths

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,12 +6,6 @@ function append_path () {
   export PATH=$PATH:$1
 }
 
-# prepend_path /usr/local/share/npm/bin
-
-# npm modules
-export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
-prepend_path /usr/local/share/npm/bin
-
 # apple put /usr/bin before /usr/local/bin -_-
 prepend_path /usr/local/bin
 prepend_path /usr/local/sbin
