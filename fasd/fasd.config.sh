ac4ecc1853645a83c9a8da757c91e2c083b0acab
magus <noah@iamnoah.com>
2025-06-16 18:21:49 -0700

    fasd: update installation instructions

--- fasd/fasd.config.sh
+++ fasd/fasd.config.sh
@@ -4,23 +4,19 @@
 # https://stackoverflow.com/questions/73586208/can-you-install-disabled-homebrew-packages
 
 echo " "
-echo "  🚨 fasd brew formula is disabled, requires manual edit"
-echo "     you will need to delete the following line from the formula"
+echo "  🚨 fasd brew formula is disabled, requires manual installation but it's easy!"
 echo " "
-echo '       disable! date: "2023-06-19", because: :repo_archived'
-echo " "
-echo " "
-echo "     Running \`brew edit fasd\` in 3 seconds..."
+echo '       https://github.com/clvv/fasd'
 echo " "
 echo " "
-sleep 3
-brew edit fasd
+echo "       git clone git@github.com:clvv/fasd.git"
+echo "       cd fasd"
+echo "       make install"
+echo " "
+read -n 1 -s -r -p "  ⏳ press any key to proceed…"
 echo " "
 echo " "
-read -n 1 -s -r -p "  ⏳ press any key to proceed after editing..."
-echo " "
-echo " "
-echo "  ✅ done, running \`brew install fasd\`..."
+echo "  ✅ done, run \`fasd\` to confirm installation"
 echo " "
 # shellcheck disable=SC2034
 HOMEBREW_NO_INSTALL_FROM_API=1 HOMEBREW_NO_AUTO_UPDATE=1 brew install fasd
